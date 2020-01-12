ecd04e0670b1deb2e6650e3a757349f712d8ec32
booldook <booldook@gmail.com>
2020-01-12 15:21:25 +0900

    api.js

--- public/js/board-api.js
+++ public/js/board-api.js
@@ -1,13 +1,23 @@
-$("#bt").click(function(){
+function getData(id) {
+	if(id) url = "/api/get/"+id;
+	else url = "/api/get/";
 	$.ajax({
-		url: "/api/get",
+		url: url,
 		type: "GET",
 		dataType: "json",
 		success: function(res) {
-			console.log(res);
+			if(id) makeList(res);
+			else viewList(res);
 		},
 		error: function(xhr) {
 			console.log(xhr);
 		}
 	});
-});
+}
+function makeList(res) {
+
+}
+function viewList(res) {
+	
+}
+getData();
