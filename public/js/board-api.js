9d30c6d73bbc866c0960a39764f4de03828d1470
booldook <booldook@gmail.com>
2020-01-12 16:21:32 +0900

    api.js

--- public/js/board-api.js
+++ public/js/board-api.js
@@ -1,3 +1,32 @@
+$("#btSave").click(function(){
+	var id = document.form1.id.value;
+	var type = "POST";
+	var url = "/api/post";
+	if(id != "") {
+		type = "PUT";
+		url = "/api/put";
+	}
+	$.ajax({
+		url: url,
+		type: type,
+		dataType: "json",
+		data: {
+			id: id,
+			title: document.form1.title.value,
+			content: document.form1.content.value,
+			writer: document.form1.writer.value,
+		},
+		success: function(res) {
+			document.form1.reset();
+			if(res.serverStatus == 2) getData();
+		},
+		error: function(xhr) {
+			console.log(xhr);
+		}
+	});
+});
+
+
 function getData(id) {
 	if(id) url = "/api/get/"+id;
 	else url = "/api/get/";
@@ -6,8 +35,8 @@ function getData(id) {
 		type: "GET",
 		dataType: "json",
 		success: function(res) {
-			if(id) makeList(res);
-			else viewList(res);
+			if(id) viewList(res);
+			else makeList(res);
 		},
 		error: function(xhr) {
 			console.log(xhr);
@@ -15,9 +44,22 @@ function getData(id) {
 	});
 }
 function makeList(res) {
-
+	console.log(res);
+	$("#title").html(res.title);
+	var html = '';
+	for(var i in res.data) {
+		html += '<tr>';
+		html += '<td>'+res.data[i].id+'</td>';
+		html += '<td>'+res.data[i].title+'</td>';
+		html += '<td>'+res.data[i].writer+'</td>';
+		html += '<td>'+res.data[i].wdate+'</td>';
+		html += '<td>'+res.data[i].rnum+'</td>';
+		html += '<td><button class="btn btn-sm btn-danger" data-id="'+res.data[i].id+'">삭제</button></td>';
+		html += '</tr>';
+	}
+	$("#listTb > tbody").html(html);
 }
 function viewList(res) {
-	
+	console.log(res);
 }
 getData();
--- router/api.js
+++ router/api.js
@@ -11,14 +11,28 @@ delete: Delete/Delete
 
 router.get(["/get", "/get/:id"], async (req, res) => {
 	let sql = '';
+	const vals = {	title: "API 게시판"	};
 	if(req.params.id) sql = "SELECT * FROM board WHERE id="+req.params.id;
 	else sql = "SELECT * FROM board ORDER BY id DESC";
 	const connect = await pool.getConnection();
 	const result = await connect.query(sql);
+	vals.data = result[0];
+	connect.release();
+	res.json(vals);
+});
+
+router.post("/post", async (req, res) => {
+	let title = req.body.title;
+	let content = req.body.content;
+	let writer = req.body.writer;
+	let wdate = new Date();
+	let sql = "INSERT INTO board SET title=?, content=?, writer=?, wdate=?";
+	let sqlVals = [title, content, writer, wdate];
+	let connect = await pool.getConnection();
+	let result = await connect.query(sql, sqlVals);
 	connect.release();
 	res.json(result[0]);
 });
-//router.post();
 //router.delete();
 //router.put();
 
